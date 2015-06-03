5b16b51f96e20987941d61a21411f97e9316ab99
David Cottrell <david_cottrell@scotiacapital.com>
2015-06-03 13:38:09 +0100

    more

---
+++ squish.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+$ echo 'a a b c a c c d' | tr ' ' '\n' | ./squish.py
+a
+b
+c
+d
+"""
+
+import sys
+
+def squish(gen):
+    """ uniquify list preserve order """
+    seen = set()
+    for x in gen:
+        if x not in seen and not seen.add(x):
+            yield x
+
+if __name__ == '__main__':
+    for x in squish(sys.stdin):
+        print(x.strip())
