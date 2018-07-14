eb729dc14774ce367c4b8c127cc63aa8816ae6c2
David Cottrell <cottrell@users.noreply.github.com>
2018-07-14 18:41:13 +0100

    update

--- clone_user.sh
+++ clone_user.sh
@@ -3,15 +3,15 @@ if [[ "$#" -ne 1 ]]; then
     echo prog.sh user
     exit 1
 fi
-curl https://api.github.com/users/$1/repos > curl.json
+curl https://api.github.com/users/$1/repos?per_page=100 > curl.json
 cat curl.json | python -c "import sys, json; d = json.loads(sys.stdin.read()); [print(','.join([str(x['fork']), x['git_url']])) for x in d]" > git_url.csv
 git init
-[[ -d forks ]] || mkdir -p forks
-cd forks
-for x in $(grep '^True' ../git_url.csv | cut -d, -f2); do
-    git submodule add $x
+# [[ -d forks ]] || mkdir -p forks
+# cd forks
+# for x in $(grep '^True' ../git_url.csv | cut -d, -f2); do
+#     git submodule add $x
+# done
+# cd -
+for x in $(grep '^False' ./git_url.csv | cut -d, -f2); do
+    git submodule add $x || :
 done
-for x in $(grep '^False' ../git_url.csv | cut -d, -f2); do
-    git submodule add $x
-done
-cd -
