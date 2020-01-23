5eb83bcd083e7333916be2aeea51f2f7de4046f0
David Cottrell <cottrell@users.noreply.github.com>
2020-01-23 21:53:25 +0000

    update

--- tunnel.sh
+++ tunnel.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
 if [[ $# -lt 2 ]]; then
-    echo usage: prog host port port
+    echo usage: prog host port port ...
     exit 1
 fi
 
 host=$1
-for x in $*; do
-    echo ssh -N -f -L localhost:$port:localhost:$port $host
-    ssh -N -f -L localhost:$port:localhost:$port $host
+shift
+for port in $*; do
+    echo ssh -v -N -f -L localhost:$port:localhost:$port $host
+    ssh -v -N -f -L localhost:$port:localhost:$port $host
 done
