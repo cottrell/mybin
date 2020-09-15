095dc11e681a4695a7ab05e904dfd2822c23863f
David Cottrell <cottrell@users.noreply.github.com>
2020-09-15 11:10:34 -0300

    update

--- _did_post.sh
+++ _did_post.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-dirname=~/Dropbox/misc/nogithub
+# dirname=~/Dropbox/misc/nogithub
+dirname=~/projects/notebooks/mybin/myhome/notes
+filename=did.txt
 # uses the name of the (link) script in UPEPR CASE as the TAG.
 # for example: ln -s _did_post.sh todo
 name=$(basename $0 | tr '[:lower:]' '[:upper:]')
-vim +'normal Go'  +'r!date +\%a\ \%d\ \%b\ \%Y\ \%H:\%M:\%S\ \%z; echo - '$name',' +'normal G$A' -- $dirname/did.txt
+vim +'normal Go'  +'r!date +\%a\ \%d\ \%b\ \%Y\ \%H:\%M:\%S\ \%z; echo - '$name',' +'normal G$A' -- $dirname/$filename
 cd $dirname
-git commit -m 'update' -a
+git add $dirname/$filename
+git commit -m 'update'
