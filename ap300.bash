864e0995c8fa519e7f6e778b54ecf2eb500147ff
David Cottrell <cottrell@users.noreply.github.com>
2020-10-02 17:46:22 -0300

    ap300 had problems with black.

--- ap300.bash
+++ ap300.bash
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 n=300
 isort $*
-black --skip-string-normalization -l $n $*
+# use black in 120 mode it mangles dictionaries otherwise? Or was this just luck in one case?
+# black --skip-string-normalization -l $n $*
 autopep8 --max-line-length=$n -i $*
 # do not write with autoflake just warn
 autoflake $*
