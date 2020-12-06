028111c7eca426c59bfb0f5c95776c155d32fe05
David Cottrell <cottrell@users.noreply.github.com>
2020-12-06 20:02:58 +0000

    lan script

--- my_lan_ip.sh
+++ my_lan_ip.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [ $(uname) = 'Darwin' ]; then
-    ipconfig getifaddr en1
+    ipconfig getifaddr en0
 else
-    hostname -I | cut -d' ' -f1
+    # hostname -I | cut -d' ' -f1
+    hostname -I | sed -e 's/\s\+$//' | rev | cut -d' ' -f1 | rev
 fi
