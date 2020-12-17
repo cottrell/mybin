7dc2abbaf78eb8f28215f4c703d52995e44c58f9
David Cottrell <cottrell@users.noreply.github.com>
2020-12-17 22:58:49 +0000

    update

--- my_lan_ip.sh
+++ my_lan_ip.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [ $(uname) = 'Darwin' ]; then
-    ipconfig getifaddr en0
+    # ipconfig getifaddr en0
+    ifconfig | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
 else
     # hostname -I | cut -d' ' -f1
     hostname -I | sed -e 's/\s\+$//' | rev | cut -d' ' -f1 | rev
