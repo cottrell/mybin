1d11099654b0e3aa605256f2a5e140830b76246d
David Cottrell <cottrell@users.noreply.github.com>
2020-12-27 18:15:59 +0000

    update

--- run_with_restart_on_error.sh
+++ run_with_restart_on_error.sh
@@ -2,7 +2,7 @@
 # TAG: keep running
 trap "kill -- -$$" EXIT
 echo "run_with_restart_on_error.sh $@ ... " >&2
-until eval "$@"; do 
+until eval "$@"; do
     echo "$(date) run_with_restart_on_error.sh $@ failed with exit code $?. Restarting ... " >&2
     sleep 1
 done
