ae596fd83897bce31c301f5f5856301685c5801f
David Cottrell <cottrell@users.noreply.github.com>
2021-01-03 19:17:31 +0000

    update

--- my_lan_ip.sh
+++ my_lan_ip.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 if [ $(uname) = 'Darwin' ]; then
     # ipconfig getifaddr en0
-    ifconfig | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
+    /sbin/ifconfig | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
 else
     # hostname -I | cut -d' ' -f1
     # hostname -I | sed -e 's/\s\+$//' | rev | cut -d' ' -f1 | rev
-    hostname -I | xargs -n 1 | grep 192 | head -1
+    /bin/hostname -I | /usr/bin/xargs -n 1 | grep 192 | /usr/bin/head -1
 fi
