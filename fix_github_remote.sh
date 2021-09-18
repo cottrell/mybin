91c0e084b983b92b5d86918350939298b6cc8696
David Cottrell <cottrell@users.noreply.github.com>
2021-09-18 20:24:46 +0100

    skip gitlab fix for now.

--- fix_github_remote.sh
+++ fix_github_remote.sh
@@ -4,6 +4,8 @@ old=$(git config --get remote.origin.url)
 echo "OLD $remote: $old"
 if $(echo $old | grep -q '^git@'); then
     echo nothing to change
+elif $(echo $old | grep -q 'gitlab'); then
+    echo is gitlab skipping ...
 else
     new=$(echo $old | sed -e 's|https://github.com/|git@github.com:|')
     cmd="git remote set-url $remote $new"
