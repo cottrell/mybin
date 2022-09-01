0f74e98eed54685360d27147d13ca685236f47f7
David Cottrell <cottrell@users.noreply.github.com>
2022-09-01 09:42:57 +0100

    update

--- remove_spaces_in_names.sh
+++ remove_spaces_in_names.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
+set -e
+
 if [[ $# -lt 1 ]]; then
-    echo usage prog files
+    echo usage prog sep files
     exit 1
 fi
+
+function parse() {
+    for arg in "$@"; do # transform long options to short ones
+        shift
+        case "$arg" in
+            "--sep") set -- "$@" "-s" ;;
+            "--verbose") set -- "$@" "-v" ;;
+            *) set -- "$@" "$arg"
+        esac
+    done
+
+    while getopts "s:v" optname  # left to ":" are flags that expect a value, right to the ":" are flags that expect nothing
+    do
+        case "$optname" in
+            "s") sep=${OPTARG} ;;
+            "v") verbose=true ;;
+        esac
+    done
+    shift "$((OPTIND-1))" # shift out all the already processed options
+}
+
+
+parse "$@"
+
+default_sep=
+sep="${sep:-$default_sep}"
 
 tmp=$(mktemp)
 something=
 for x in "$@"; do
     echo $x | grep -q ' ' || continue
-    dest=$(echo $x | sed 's/ //g')
+    dest=$(echo $x | sed "s/ /$sep/g")
     echo mv \"$x\" \"$dest\" >> $tmp
     something="something"
 done
