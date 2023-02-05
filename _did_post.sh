612df4bcb8b5f062916ea6391a371ea36920c2ce
David Cottrell <cottrell@users.noreply.github.com>
2023-02-05 18:58:35 +0000

    Change background colour on remote.

--- _did_post.sh
+++ _did_post.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # dirname=~/Dropbox/misc/nogithub
 dirname=~/projects/notebooks/mybin/myhome/notes
-filename=did.txt
+hostname=$(hostname)
+filename=did_$hostname.txt
 # uses the name of the (link) script in UPEPR CASE as the TAG.
 # for example: ln -s _did_post.sh todo
 name=$(basename $0 | tr '[:lower:]' '[:upper:]')
-vim +'normal Go'  +'r!date +\%a\ \%d\ \%b\ \%Y\ \%H:\%M:\%S\ \%z; echo - '$name',' +'normal G$A' -- $dirname/$filename
+vim +'normal Go'  +'r!date +\%a\ \%d\ \%b\ \%Y\ \%H:\%M:\%S\ \%z; echo - '$name,$hostname',' +'normal G$A' -- $dirname/$filename
 cd $dirname
 git pull
 git add $dirname/$filename
