887277f48772b50653584c7ccc4a718ce3946fef
David Cottrell <cottrell@users.noreply.github.com>
2023-09-29 10:36:14 +0100

    Try this did post thing background.

--- _did_post.sh
+++ _did_post.sh
@@ -7,8 +7,13 @@ filename=did_$hostname.txt
 # for example: ln -s _did_post.sh todo
 name=$(basename $0 | tr '[:lower:]' '[:upper:]')
 vim +'normal Go'  +'r!date +\%a\ \%d\ \%b\ \%Y\ \%H:\%M:\%S\ \%z; echo - '$name,$hostname',' +'normal G$A' -- $dirname/$filename
-cd $dirname
-git pull
-git add $dirname/$filename
-git commit -m 'update'
+# 2023-09-29 try run things in background and disown. Consider disabling pull
+# and push constantly as well now that I have partitioned by device.
+(
+cd $dirname && \
+git pull && \
+git add $dirname/$filename && \
+git commit -m 'update' && \
 git push
+) &
+disown
