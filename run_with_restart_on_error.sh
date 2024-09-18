bbb18e409b37f55b7816c57072ad6f80aefa6772
David Cottrell <cottrell@users.noreply.github.com>
2024-09-18 17:55:00 +0100

    Update run with restart to take sleep time.

--- run_with_restart_on_error.sh
+++ run_with_restart_on_error.sh
@@ -1,8 +1,37 @@
 #!/bin/sh
 # TAG: keep running
+
+# Set default sleep time (in seconds)
+sleep_time=1
+
+# Parse arguments
+while getopts ":s:" opt; do
+  case $opt in
+    s) sleep_time="$OPTARG"
+       ;;
+    \?) echo "Invalid option: -$OPTARG" >&2
+       exit 1
+       ;;
+  esac
+done
+
+# Shift arguments to avoid processing them as options
+shift $(($OPTIND - 1))
+
+# Main loop
 trap "kill -- -$$" EXIT
 echo "run_with_restart_on_error.sh $@ ... " >&2
 until eval "$@"; do
-    echo "$(date) run_with_restart_on_error.sh $@ failed with exit code $?. Restarting ... " >&2
-    sleep 1
+  echo "$(date) run_with_restart_on_error.sh $@ failed with exit code $?. Sleeping for $sleep_time s then restarting ... " >&2
+  sleep "$sleep_time"
 done
+
+# # TAG: keep running
+# trap "kill -- -$$" EXIT
+# echo "run_with_restart_on_error.sh $@ ... " >&2
+# until eval "$@"; do
+#     echo "$(date) run_with_restart_on_error.sh $@ failed with exit code $?. Restarting ... " >&2
+#     sleep 1
+# done
+
+
