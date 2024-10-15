ff8578eab3cf35b87b5c704d222a594d8818e621
David Cottrell <cottrell@users.noreply.github.com>
2024-10-15 17:53:31 +0100

    update from bleepblop

--- ap120.bash
+++ ap120.bash
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 n=120
+type pylint | pip install isort black pylint
 isort --line-length=$n --wrap-length=$n $*
 black --skip-string-normalization -l $n $*
 # autopep8 --max-line-length=$n -i $*
