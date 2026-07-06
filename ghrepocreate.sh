79c19f68349ae07ca007bdd07bd45f096273669c
David Cottrell <cottrell@users.noreply.github.com>
2026-07-06 13:21:04 +0100

    update from bleepblop

--- ghrepocreate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <repository-name>"
-    exit 1
-fi
-
-echo "Running: gh repo create $1 --source=. --private --push"
-gh repo create $1 --source=. --private --push
