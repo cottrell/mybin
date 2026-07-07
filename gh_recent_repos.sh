f236bd78b00209adcf8d319c89bd6235cf313c90
David Cottrell <cottrell@users.noreply.github.com>
2026-07-07 22:19:22 +0100

    update from bleepblop

--- gh_recent_repos.sh
+++ gh_recent_repos.sh
@@ -4,7 +4,7 @@ TODAY=$(date +%Y-%m-%d)
 DATE_ONE_YEAR_AGO=$(date -d "$TODAY - 1 year" +%Y-%m-%d)
 
  (
-  gh api -H "Accept: application/vnd.github+json" "/search/repositories?q=user:cottrell+pushed:>=$DATE_ONE_YEAR_AGO&per_page=1000" --jq '.items[].full_name'
-  gh api -H "Accept: application/vnd.github+json" "/search/repositories?q=user:cottrell+created:>=$DATE_ONE_YEAR_AGO&per_page=1000" --jq '.items[].full_name'
+  gh api -H "Accept: application/vnd.github+json" "/search/repositories?q=user:cottrell+pushed:>=$DATE_ONE_YEAR_AGO&per_page=1000" --jq '.items[] | "\(.full_name)\t\(if .private then "private" else "public" end)"'
+  gh api -H "Accept: application/vnd.github+json" "/search/repositories?q=user:cottrell+created:>=$DATE_ONE_YEAR_AGO&per_page=1000" --jq '.items[] | "\(.full_name)\t\(if .private then "private" else "public" end)"'
 ) | sort -u
 
